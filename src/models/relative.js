bb15054a94f0b699079ef645f72ef0d666544a22
Estuardo-Sazo <estuardosazo1206@gmail.com>
2021-05-10 13:42:34 -0600

    Cambios antes

---
+++ src/models/relative.js
@@ -0,0 +1,13 @@
+class Relative {
+    constructor() {
+        this.nt = 0;
+        this.t = 0;
+        this.r = 0;
+        this.no = 0;
+        this.medicion = '';
+        this.poblation = '';
+    }
+
+
+
+}
